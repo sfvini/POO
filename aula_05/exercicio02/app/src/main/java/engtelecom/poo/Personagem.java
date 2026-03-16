437452d0e510ecdcdd86009e3fa9e977800b4fcf
sfvini <vinicius.sf2007@aluno.ifsc.edu.br>
2026-03-16 09:12:13 -0300

    aula 05/ 2

---
+++ aula_05/exercicio02/app/src/main/java/engtelecom/poo/Personagem.java
@@ -0,0 +1,20 @@
+package engtelecom.poo;
+
+public class Personagem {
+    private String classe;
+    private int xp;
+    private int hp;
+    private int dano;
+    private int velocidade;
+
+    public void setPersonagem(String c, int x, int h, int d, int v){
+        classe = c;
+        xp = x;
+        hp = h;
+        dano = d;
+        velocidade = v;
+    }
+
+
+}
+
