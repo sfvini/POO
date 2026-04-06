f86b1f976e50c0fec3aeeb5e53fa9cf0af557ce1
sfvini <vinicius.sf2007@aluno.ifsc.edu.br>
2026-04-06 09:17:40 -0300

    aula 08 (falta terminar)

---
+++ aula_08/app/src/main/java/engtelecom/poo/Livro.java
@@ -0,0 +1,53 @@
+package engtelecom.poo;
+
+public class Livro {
+    private final String isbn;
+    private String titulo;
+    private String autor;
+    private int ano;
+
+    public Livro(String isbn, String titulo, String autor, int ano) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                ISBN: %s
+                Título: %s
+                Autor: "%s
+                Ano: %d
+                """, isbn, titulo, autor, ano);
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
