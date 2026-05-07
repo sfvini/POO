81bf984826881bbebf6394befbf77ca41de0264d
sfvini <vinicius.sf2007@aluno.ifsc.edu.br>
2026-05-07 09:14:15 -0300

    lab 5 - começo do Display

--- lab_05/app/src/main/java/engtelecom/poo/Display.java
+++ lab_05/app/src/main/java/engtelecom/poo/Display.java
@@ -1,4 +1,34 @@
 package engtelecom.poo;
 
+import edu.princeton.cs.algs4.Draw;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Display {
+    private ArrayList<Segmento> segmentos;
+    private int numero;
+
+    public Display(double x, double y, double tamanho){
+        this.segmentos = new ArrayList<>();
+        this.numero = 0;
+
+        // fazer lógica para criar os 7 segmentos
+
+        setValor(0);
+    }
+
+    public void setValor(int numero){
+        if (numero >= 0 && numero <= 9) {
+            this.numero = numero;
+            // fazer lógica para ligar cada segmento conforme número (loop)
+        }
+    }
+
+    public void desenhar(Draw desenho, Color cor){
+        segmentos.forEach(segmento -> {
+            segmento.desenhar(desenho, cor);
+        });
+    }
+
 }
