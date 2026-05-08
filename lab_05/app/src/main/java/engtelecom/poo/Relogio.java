a6e7484a99047c8f8879a2d451a85abf6d1f9fa8
Vinícius França <vinicius.sf2007@aluno.ifsc.edu.br>
2026-05-08 18:20:32 -0300

    Add fields and imports to Relogio class

--- lab_05/app/src/main/java/engtelecom/poo/Relogio.java
+++ lab_05/app/src/main/java/engtelecom/poo/Relogio.java
@@ -1,4 +1,16 @@
 package engtelecom.poo;
 
+import java.util.ArrayList;
+import edu.princeton.cs.algs4.Draw;
+import java.awt.*;
+
 public class Relogio {
+  private ArrayList<Display> displays;
+  private int hora;
+  private int minuto;
+  private int segundo;
+  private Color cor;
+  private double x;
+  private double y;
+  private double tamanho;
 }
